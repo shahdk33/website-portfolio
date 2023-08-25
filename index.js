484d09494e217abc1f49577b23b37352ff8b7bbd
Shahd Khartabil <sh977589@dal.ca>
2023-08-25 12:03:35 -0300

    dark mode initial - for body, nav, and about me

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function openVideo(url){
   window.open(url, '_blank');
 
 }
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  
+  var nav = document.getElementById('navigation');
+  nav.classList.toggle("dark-mode");
+
+  var text = document.getElementById("regular");
+  text.classList.toggle("dark-mode");
+
+  var about = document.getElementById("aboutMe");
+  about.classList.toggle("dark-mode");
+
+}
