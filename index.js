72c5110cd8ef7373e514a4a28bf29d3dde0433fd
Shahd Khartabil <sh977589@dal.ca>
2023-11-15 20:52:08 -0400

    dark mode toggle fix + gradient

--- index.js
+++ index.js
@@ -16,32 +16,6 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 
-  var nav = document.getElementById('navigation');
-  nav.classList.toggle("dark-mode");
-
-  var text = document.getElementById("regular");
-  text.classList.toggle("dark-mode");
-
-  var about = document.getElementById("aboutMe");
-  about.classList.toggle("dark-mode");
-
-  var contacts = document.getElementById("mycontacts");
-  contacts.classList.toggle("dark-mode");
-
-  var projects = document.getElementsByClassName("projectscontainer");
-  for (var i = 0; i < projects.length; i++) {
-    projects[i].classList.toggle("dark-mode");
-  }
-
-  var skills = document.getElementById("skillsLogos");
-  skills.classList.toggle("dark-mode");
-
-  var dark = document.getElementById("toggleDark");
-  dark.classList.toggle("dark-mode");
-  
-  var dark = document.getElementById("menuContainer");
-  dark.classList.toggle("dark-mode");
-
   var toggleButton = document.getElementById('toggleDark');
   if (element.classList.contains("dark-mode")) {
     toggleButton.textContent = "☀️ light mode";
